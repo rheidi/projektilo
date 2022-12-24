639f378314b60d0e8e9193c8df3b3badeb900f73
rheidi <heidi@DESKTOP-7JR1TGL.localdomain>
2022-12-24 16:20:45 +0200

    data matkalla palvelimelle

--- part1/src/App.js
+++ part1/src/App.js
@@ -11,11 +11,15 @@ const Title = (props) => {
 const Lista = ({ yarns }) => {
   return (
     <table>
-      <tr>
-        <th>Langan nimi:</th>
-        <th>Langan vahvuus:</th>
-      </tr>
-      {yarns.map(yarn => <Yarn yarn={yarn} />)}
+      <thead>
+        <tr>
+          <th>Langan nimi:</th>
+          <th>Langan vahvuus:</th>
+        </tr>
+      </thead>
+      <tbody>
+        {yarns.map(yarn => <Yarn key={yarn.id} yarn={yarn} />)}
+      </tbody>
     </table>
   )
   
@@ -31,53 +35,12 @@ const Yarn = (props) => {
   )
 }
 
-const App = () => {
+const App = ({ yarns, anecdotes }) => {
   const otsikko = 'Villa on parasta'
-  const yarns = [
-    {
-      id: 1,
-      name: 'Fabel',
-      weight: 50,
-      meterage: 205,
-    },
-    {
-      id: 2,
-      name: '7 Veljestä',
-      weight: 100,
-      meterage: 200,
-    },
-    {
-      id: 3,
-      name: 'Merino Sock',
-      weight: 100,
-      meterage: 425,
-    },
-  ]  
-  const anecdotes = [
-    'Hyvin suunniteltu on puoliksi neulottu.',
-    'Älä jätä huomiseksi sitä, jonka voit neuloa jo tänään.',
-    'Neulehetki kullan kallis.',
-    'Kell\' lankaa on, se langan kätkeköön.',
-    'Parempi pyöröpuikko pipossa kuin kymmenen villapaidassa.',
-    'Itku pitkästä purkamisesta, pieru paljon neulomisesta.',
-    'Purkaminen on opintojen äiti.',
-    'Seuraa kuin neuloja lanka-alennuksia.',
-    'Nyt on langat vähissä.',
-    'Ei ole nauleita silmukoihin katsominen.',
-    'Ei virkkuukoukku kauas sohvasta putoa.',
-    'Pyörivä kerä ei sammaloidu.',
-    'Jos kahvi, suklaa ja käsityöt ei auta, niin tauti on kuolemaksi.',
-    'Kyllä käsityöt ihmisen kotiin ajaa.',
-    'Moni neule päältä kaunis.',
-    'Ei kannata mennä sohvaa edemmäs neulomaan.',
-    'Neulojan lapsella ei ole villasukkia.',
-    'Sitä saa mitä neuloo.',
-  ]
 
   const [selected, setSelected] = useState(0)
 
-  const increaseByOne = () => setSelected(selected +1)
-  const setToZero = () => setSelected(0)
+  const setRandom = () => setSelected(Math.floor(Math.random() * (anecdotes.length +1)))
 
   return (
     <div>
@@ -85,11 +48,8 @@ const App = () => {
       <Lista yarns={yarns} />
       <br />
       {anecdotes[selected]}<br />
-      <button onClick={increaseByOne}>
-        seuraava
-      </button>
-      <button onClick={setToZero}>
-        aloita alusta
+      <button onClick={setRandom}>
+        satunnainen
       </button>
     </div>
   )
--- part1/src/index.js
+++ part1/src/index.js
@@ -3,4 +3,48 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const yarns = [
+  {
+    id: 1,
+    name: 'Fabel',
+    weight: 50,
+    meterage: 205,
+  },
+  {
+    id: 2,
+    name: '7 Veljestä',
+    weight: 100,
+    meterage: 200,
+  },
+  {
+    id: 3,
+    name: 'Merino Sock',
+    weight: 100,
+    meterage: 425,
+  },
+]
+
+const anecdotes = [
+  'Hyvin suunniteltu on puoliksi neulottu.',
+  'Älä jätä huomiseksi sitä, jonka voit neuloa jo tänään.',
+  'Neulehetki kullan kallis.',
+  'Kell\' lankaa on, se langan kätkeköön.',
+  'Parempi pyöröpuikko pipossa kuin kymmenen villapaidassa.',
+  'Itku pitkästä purkamisesta, pieru paljon neulomisesta.',
+  'Purkaminen on opintojen äiti.',
+  'Seuraa kuin neuloja lanka-alennuksia.',
+  'Nyt on langat vähissä.',
+  'Ei ole nauleita silmukoihin katsominen.',
+  'Ei virkkuukoukku kauas sohvasta putoa.',
+  'Pyörivä kerä ei sammaloidu.',
+  'Jos kahvi, suklaa ja käsityöt ei auta, niin tauti on kuolemaksi.',
+  'Kyllä käsityöt ihmisen kotiin ajaa.',
+  'Moni neule päältä kaunis.',
+  'Ei kannata mennä sohvaa edemmäs neulomaan.',
+  'Neulojan lapsella ei ole villasukkia.',
+  'Sitä saa mitä neuloo.',
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App yarns={yarns} anecdotes={anecdotes} />
+)
